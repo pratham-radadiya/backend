3cec2eb022e7882ff3f1c956f0a5d686120e28b9
pratham radadiya <prathamradadiya027@gmail.com>
2025-05-30 22:47:23 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 // app.js
 import express from 'express'; 
-const app = express();  // or whatever your app is
+const app = express();  
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+
+
+
+
 export default app;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asycHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asycHandler}
+
