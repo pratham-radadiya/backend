33da114fc2f4651c4ee086b8c31beceeff326dda
pratham radadiya <prathamradadiya027@gmail.com>
2025-05-31 20:54:17 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; 
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+     email: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+     fullName: {
+        type: String,
+        require: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,
+        require: true
+    },
+    coverImage: {
+        type: String
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "video"
+        }
+    ],
+    password: {
+        type: String,
+        require: [true, "passwrod is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+
+
+}, {timestamps: ture})
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.pasword, 10)
+    next()
+})
+
+userSchema.method.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS.TOKEN.EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
