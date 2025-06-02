b3622f8184d399ea2ff6ad91818576a95338e4a2
pratham radadiya <prathamradadiya027@gmail.com>
2025-06-02 07:26:04 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // app.js
 import express from 'express'; 
+import cors from 'cors';
 const app = express();  
 
 app.use(cors({
@@ -10,6 +11,10 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+
+import userRouter from './routes/user.routes.js';
+
+app.use("/api/v1/users", userRouter)
 
 
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,9 @@
-const asycHandler = (requestHandler) => {
-    (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
 }
 
-export {asycHandler}
+export  {asyncHandler}
 
